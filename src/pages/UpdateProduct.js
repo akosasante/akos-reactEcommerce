2402037a0b4d6c749bbc5e4341409ece93e42fd5
Natacha Key <nataliaklueva@hotmail.com>
2023-06-27 14:06:18 +0200

    styles added for crud

--- src/pages/UpdateProduct.js
+++ src/pages/UpdateProduct.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import './styles.css';
 
 const UpdateProduct = ({ productId }) => {
   const [name, setName] = useState('');
@@ -56,34 +57,41 @@ const UpdateProduct = ({ productId }) => {
   };
 
   return (
-    <div>
-      <h2>Update Product</h2>
+    <div className='center'>
+    <div >
+      <h3>Update Product</h3>
       <form onSubmit={handleSubmit}>
       <div>
-          <label>Name</label>
+          <label>Name </label>
+          <div>
           <input
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+          </div>
         </div>
         <div>
-          <label>Price</label>
+          <label>Price </label>
+          <div>
           <input
             type="number"
             value={price}
             onChange={(e) => setPrice(e.target.value)}
           />
+          </div>
         </div>
         <div>
-          <label>Description</label>
+          <label>Description </label>
+          <div>
           <textarea
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           ></textarea>
+          </div>
         </div>
         <div>
-          <label>Category</label>
+          <label>Category </label>
           <select
             value={category}
             onChange={(e) => setCategory(e.target.value)}
@@ -95,7 +103,7 @@ const UpdateProduct = ({ productId }) => {
           </select>
         </div>
         <div>
-          <label>Company</label>
+          <label>Company </label>
           <select value={company} onChange={(e) => setCompany(e.target.value)}>
             <option value="">Select Company</option>
             <option value="Suarez">Suarez</option>
@@ -104,7 +112,7 @@ const UpdateProduct = ({ productId }) => {
           </select>
         </div>
         <div>
-          <label>Featured</label>
+          <label>Featured </label>
           <input
             type="checkbox"
             checked={featured}
@@ -112,7 +120,7 @@ const UpdateProduct = ({ productId }) => {
           />
         </div>
         <div>
-          <label>Free Shipping</label>
+          <label>Free Shipping </label>
           <input
             type="checkbox"
             checked={freeShipping}
@@ -120,22 +128,32 @@ const UpdateProduct = ({ productId }) => {
           />
         </div>
         <div>
-          <label>Inventory</label>
+          <label>Inventory </label>
+          <div>
           <input
             type="number"
             value={inventory}
             onChange={(e) => setInventory(e.target.value)}
           />
+          </div>
         </div>
-        <button type="submit" onClick={handleSubmit}>
+        {/* <button type="submit" onClick={handleSubmit}>
           Update Product
-        </button>
+        </button> */}
+        <button type="submit" className='crudbtn' onClick={handleSubmit}>
+    <span>Update Product</span>
+    <svg width="34" height="34" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="37" cy="37" r="35.5" stroke="black" stroke-width="3"></circle>
+        <path d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z" fill="black"></path>
+    </svg>
+</button>
       </form>
       <p>
         <b>
           <em>{message}</em>
         </b>
       </p>
+    </div>
     </div>
   );
 };
