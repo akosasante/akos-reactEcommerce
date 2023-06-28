fb7d50f0ec1412d674e072d066afdf92eeb7c481
Natacha Key <nataliaklueva@hotmail.com>
2023-06-28 18:19:36 +0200

    order updated - still product undefined

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -5,73 +5,62 @@ import axios, { AxiosError } from 'axios';
 const rootUrl = 'https://ecommerce-6kwa.onrender.com';
 
 const Orders = () => {
-
-  //fetch current user
   const [currentUser, setCurrentUser] = useState(null);
-  const [message, setMessage] = useState(''); 
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     async function fetchData() {
       const url = `${rootUrl}/api/v1/users/showMe`;
-      axios
-        .get(url, { withCredentials: true })
-        .then((response) => {
-          setCurrentUser(response?.data?.user);
-        })
-        .catch((error) => {
-          const errorPayload =
-            error instanceof AxiosError ? error?.response?.data : error;
-          console.error(errorPayload);
-        });
+
+      try {
+        const response = await axios.get(url, { withCredentials: true });
+        setCurrentUser(response?.data?.user);
+      } catch (error) {
+        const errorPayload =
+          error instanceof AxiosError ? error?.response?.data : error;
+        console.error(errorPayload);
+      }
     }
     fetchData();
   }, []);
-const user= currentUser;
 
-//SINGLE ITEM ORDER
+  const user = currentUser;
 
-
-
-
-//WHOLE ORDER CHECKOUT
-
-  const { cart, clearCart } = useCartContext();
-  const { total_amount, shipping_fee } = useCartContext();
-   const [tax, setTax] = useState(10);
-  
+  const { cart, clearCart, total_amount, shipping_fee } = useCartContext();
+  const [tax, setTax] = useState(10);
   const [order, setOrder] = useState(null);
 
   const handleCheckout = async () => {
-   
-        try {
-          const response = await axios.post(
-            'https://ecommerce-6kwa.onrender.com/api/v1/orders',
-            {
-              tax,
-              shippingFee: shipping_fee,
-              subtotal: total_amount,
-              total: total_amount + shipping_fee + tax,
-              items: cart,
-              user: user.userId,
-              clientSecret: 'randomString',
-            },
-            { withCredentials: true },
-            {
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            }
-          );
-          console.log('Order added successfully:', response.data);
-          setMessage(`Order added successfully`);
-          setOrder(response.data.order);
-          
-        } catch (error) {
-          console.error('Error:', error.message);
+    try {
+      const response = await axios.post(
+        'https://ecommerce-6kwa.onrender.com/api/v1/orders',
+        {
+          tax,
+          shippingFee: shipping_fee,
+          subtotal: total_amount,
+          total: total_amount + shipping_fee + tax,
+          items: cart,
+          user: user.userId,
+          clientSecret: 'randomString',
+        },
+        {
+          withCredentials: true,
+          headers: {
+            'Content-Type': 'application/json',
+          },
         }
-      };
- 
+      );
+
+      setOrder(response.data.order);
+      console.log('Order added successfully:', response.data);
+      setMessage(`Order added successfully`);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
   return (
-    <div className="center ">
+    <div className="center">
       <div className="space">
         <h4>Cart Items:</h4>
         {cart.length === 0 ? (
@@ -80,9 +69,7 @@ const user= currentUser;
           <ul>
             {cart.map((item, index) => (
               <li key={index}>
-                
-                Product: {item.name} (  quantity: {item.amount} ) * ${item.price}
-                
+                Product: {item.name} (quantity: {item.amount}) * ${item.price}
               </li>
             ))}
           </ul>
@@ -97,7 +84,6 @@ const user= currentUser;
       </div>
 
       <div>
-        {/* <button onClick={handleCheckout}>Place Order</button> */}
         <button type="submit" className="crudbtn" onClick={handleCheckout}>
           <span>Place Order</span>
           <svg
@@ -112,7 +98,7 @@ const user= currentUser;
               cy="37"
               r="35.5"
               stroke="black"
-              stroke-width="3"
+              strokeWidth="3"
             ></circle>
             <path
               d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
