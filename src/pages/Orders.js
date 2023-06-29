3be0da946d980673aee5c4c5a7c72449d5a0b76f
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-06-29 08:48:48 -0400

    fix place order and hide order button once successful

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -7,6 +7,7 @@ const rootUrl = 'https://ecommerce-6kwa.onrender.com';
 const Orders = () => {
   const [currentUser, setCurrentUser] = useState(null);
   const [message, setMessage] = useState('');
+  const [hasOrdered, setHasOrdered] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -26,7 +27,7 @@ const Orders = () => {
 
   const user = currentUser;
 
-  const { cart, clearCart, total_amount, shipping_fee } = useCartContext();
+  const { cart, total_amount, shipping_fee } = useCartContext();
   const [tax, setTax] = useState(10);
   const [order, setOrder] = useState(null);
 
@@ -54,6 +55,7 @@ const Orders = () => {
       setOrder(response.data.order);
       console.log('Order added successfully:', response.data);
       setMessage(`Order added successfully`);
+      setHasOrdered(true);
     } catch (error) {
       console.error('Error:', error.message);
     }
@@ -84,28 +86,30 @@ const Orders = () => {
       </div>
 
       <div>
-        <button type="submit" className="crudbtn" onClick={handleCheckout}>
-          <span>Place Order</span>
-          <svg
-            width="34"
-            height="34"
-            viewBox="0 0 74 74"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="37"
-              cy="37"
-              r="35.5"
-              stroke="black"
-              strokeWidth="3"
-            ></circle>
-            <path
-              d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
-              fill="black"
-            ></path>
-          </svg>
-        </button>
+        {!hasOrdered && (
+          <button type="submit" className="crudbtn" onClick={handleCheckout}>
+            <span>Place Order</span>
+            <svg
+              width="34"
+              height="34"
+              viewBox="0 0 74 74"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                cx="37"
+                cy="37"
+                r="35.5"
+                stroke="black"
+                strokeWidth="3"
+              ></circle>
+              <path
+                d="M25 35.5C24.1716 35.5 23.5 36.1716 23.5 37C23.5 37.8284 24.1716 38.5 25 38.5V35.5ZM49.0607 38.0607C49.6464 37.4749 49.6464 36.5251 49.0607 35.9393L39.5147 26.3934C38.9289 25.8076 37.9792 25.8076 37.3934 26.3934C36.8076 26.9792 36.8076 27.9289 37.3934 28.5147L45.8787 37L37.3934 45.4853C36.8076 46.0711 36.8076 47.0208 37.3934 47.6066C37.9792 48.1924 38.9289 48.1924 39.5147 47.6066L49.0607 38.0607ZM25 38.5L48 38.5V35.5L25 35.5V38.5Z"
+                fill="black"
+              ></path>
+            </svg>
+          </button>
+        )}
         <p>{message}</p>
       </div>
 
